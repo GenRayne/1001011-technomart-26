a89d11c84d0bd4ed7c3f48c0c44bbf260fb7b85b
GenRayne <gengakutaku@gmail.com>
2019-10-09 23:55:11 +0700

    Скрипт подключен в каталог, добавлен try catch и закрытие модального в каталоге

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
-var mapPicBtn = document.querySelector('.map-link'),
-    mapModal = document.querySelector('.map-modal'),
-    closeMap = mapModal.querySelector('.close-btn');
+// Модальное окно с картой
 
-var writeBtn = document.querySelector('.write-us-btn'),
-    writeModal = document.querySelector('.write-us-modal'),
-    closeMessage = writeModal.querySelector('.close-btn');
+try {
+  var mapPicBtn = document.querySelector('.map-link'),
+      mapModal = document.querySelector('.map-modal'),
+      closeMap = mapModal.querySelector('.close-btn');
 
-mapPicBtn.addEventListener('click', function (ev) {
-  ev.preventDefault();
-  mapModal.classList.remove('hidden');
-});
+  mapPicBtn.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    mapModal.classList.remove('hidden');
+  });
 
-closeMap.addEventListener('click', function (ev) {
-  ev.preventDefault();
-  mapModal.classList.add('hidden');
-});
+  closeMap.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    mapModal.classList.add('hidden');
+  });
+} catch (err) {
+  console.log(err);
+}
 
-writeBtn.addEventListener('click', function (ev) {
-  ev.preventDefault();
-  writeModal.classList.remove('hidden');
-});
+// Модальное окно "Напишите нам"
 
-closeMessage.addEventListener('click', function (ev) {
-  ev.preventDefault();
-  writeModal.classList.add('hidden');
-});
+try {
+  var writeBtn = document.querySelector('.write-us-btn'),
+      writeModal = document.querySelector('.write-us-modal'),
+      closeMessage = writeModal.querySelector('.close-btn');
+
+  writeBtn.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    writeModal.classList.remove('hidden');
+  });
+
+  closeMessage.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    writeModal.classList.add('hidden');
+  });
+} catch (err) {
+  console.log(err);
+}
+
+// Модальное окно "Товар добавлен в корзину"
+
+try {
+  var butBtns = document.querySelectorAll(".buy-btn"),
+      addedToCartModal = document.querySelector(".added-to-cart"),
+      closeAddedToCart = addedToCartModal.querySelector(".close-btn");
+
+  closeAddedToCart.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    addedToCartModal.classList.add('hidden');
+  });
+} catch (err) {
+  console.log(err);
+}
